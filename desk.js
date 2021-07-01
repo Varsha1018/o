2bb724a4eab7482edb14cdafb9e8c0ada04346d5
Varsha1018 <76804038+Varsha1018@users.noreply.github.com>
2021-07-01 17:59:31 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,35 @@
+img="";
+
+function preload(){
+    img=loadImage("desk.webp");
+}
+
+function setup(){
+    canvas=createCanvas(640, 420);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd", modelloaded)
+    document.getElementById("status").innerHTML="Status - Detecting Object";
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+
+status="";
+
+function modelloaded(){
+    console.log("Model Loaded!!");
+    status=true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+    console.error(error);
+    }
+    console.log(results);
+}
+
+function back(){
+    window.location="index.html";
+}
